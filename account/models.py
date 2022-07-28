f2419d543a7453235783937d1dc006ce8943c19b
Ali <hasanzadehali214@gmail.com>
2022-07-28 21:35:16 +0430

    user profile added

--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
+from distutils.command.upload import upload
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(default='profile_photos/default.jpg', upload_to='profile_photos')
