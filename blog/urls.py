492050f072699b13bf674d8d7217fd1a56531de4
Ali <hasanzadehali214@gmail.com>
2022-11-03 20:04:15 +0330

    post crud added(class based)

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from blog import views as blog_views
+from .views import PostList, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', blog_views.home, name='blog-home'),
-    path('about/', blog_views.about, name='blog-about')
+    path('', PostList.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', blog_views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,70 @@
-from multiprocessing.spawn import import_main_path
-from turtle import pos
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
 
-def home(request):
-    posts = Post.objects.all()
-    context = {
-        'title': 'home',
-        'posts':  posts
-    }
+# def home(request):
+#     posts = Post.objects.all()
+#     context = {
+#         'title': 'home',
+#         'posts':  posts
+#     }
 
-    return render(request, 'blog/home.html', context)
+#     return render(request, 'blog/home.html', context)
 
 
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_added']
+    
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post-detail.html'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        print(post)
+        if post.author == self.request.user:
+            return True
+        else:
+            return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        print(post)
+        if post.author == self.request.user:
+            return True
+        else:
+            return False
+
+    
 def about(request):
     context = {
         'title': 'about'
